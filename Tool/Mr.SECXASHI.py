08c442cd009a4790dabb5409c42e94af5c432c2d
Mr.SECXASHI <62788900+ZeNKoKu@users.noreply.github.com>
2021-06-23 18:05:32 +0700

    Update Mr.SECXASHI.py

--- Tool/Mr.SECXASHI.py
+++ Tool/Mr.SECXASHI.py
@@ -11,7 +11,7 @@ os.system("clear")
 os.system("figlet -f mono9 Mr.SECXASHI | lolcat -a")
 os.system("banner Script created by Mr.SECXASHI")
 print("https://github.com/ZeNKoKu")
-print("Version 0.0.2")
+print("Version 0.0.1")
 print("")
 print("")
 print("1.All Tool Hacking")
@@ -19,8 +19,6 @@ print("To exit, type exit.")
 all = input("select : ")
 if all == "1" or all == "01":
     print("1.Web hacking")
-if all == "2" or all == "01":
-    print("2.Hack Server")
 
 Tool = input("select : ")
 if Tool == "1" or Tool == "01":
@@ -28,10 +26,6 @@ if Tool == "1" or Tool == "01":
     print("01.Sqlmap")
     print("02.Nmap")
     print("03.Hydra")
-if Tool == "2" or Tool == "02":
-    print("01.Nmap")
-    print("02.Hydra")
-    print("03.Rockyou.txt")
 print("")
 print("Which one do you want to install ?")
 Tool2 = input("select : ")
@@ -39,15 +33,8 @@ Tool2 = input("select : ")
 if Tool2 == "1" or Tool2 == "01":
     os.system("pip install sqlmap")
 if Tool2 == "2" or Tool2 == "02":
-    os.system("pip install nmap")
+    os.system("pkg install nmap")
 if Tool2 == "3" or Tool2 == "03":
     os.system("pip install hydra")
-if Tool2 == "1" or Tool3 == "01"
-    os.system("pip install Nmap")
-if Tool2 == "2" or Tool3 == "02"
-    os.system("pip install Hydra")
-if Tool2 == "3" or Tool3 == "03"
-    os.system("wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt")
 print("")
 print("")
-print("Thank")
