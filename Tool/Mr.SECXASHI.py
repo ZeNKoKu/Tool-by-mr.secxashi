85a5aca5fe625a171f3b59a27a1048ffb2cce1e5
Mr.SECXASHI <62788900+ZeNKoKu@users.noreply.github.com>
2021-06-23 17:53:11 +0700

    Update Mr.SECXASHI.py

--- Tool/Mr.SECXASHI.py
+++ Tool/Mr.SECXASHI.py
@@ -19,6 +19,8 @@ print("To exit, type exit.")
 all = input("select : ")
 if all == "1" or all == "01":
     print("1.Web hacking")
+if all == "2" or all == "01":
+    print("2.Hack Server")
 
 Tool = input("select : ")
 if Tool == "1" or Tool == "01":
@@ -26,6 +28,10 @@ if Tool == "1" or Tool == "01":
     print("01.Sqlmap")
     print("02.Nmap")
     print("03.Hydra")
+if Tool == "2" or Tool == "02":
+    print("01.Nmap")
+    print("02.Hydra")
+    print("03.Rockyou.txt")
 print("")
 print("Which one do you want to install ?")
 Tool2 = input("select : ")
@@ -36,6 +42,12 @@ if Tool2 == "2" or Tool2 == "02":
     os.system("pip install nmap")
 if Tool2 == "3" or Tool2 == "03":
     os.system("pip install hydra")
+if Tool3 == "1" or Tool3 == "03"
+    os.system("pip install Nmap")
+if Tool3 == "2" or Tool3 == "02"
+    os.system("pip install Hydra")
+if Tool3 == "3" or Tool3 == "03"
+    os.system("wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt")
 print("")
 print("")
 print("Thank")
